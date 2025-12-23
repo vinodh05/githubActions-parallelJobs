47c6704c22131e7265140d8156f773077421fe71
vinodh05 <154605978+vinodh05@users.noreply.github.com>
2025-12-23 14:03:26 +0530

    Update testGreet to check greeting for 'Bob'

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -4,6 +4,6 @@ import static org.junit.Assert.*;
 public class AppTest {
     @Test
     public void testGreet() {
-        assertEquals("Hello, Alice!", App.greet("Alice"));
+        assertEquals("Hello, Alice!", App.greet("Bob"));
     }
 }
